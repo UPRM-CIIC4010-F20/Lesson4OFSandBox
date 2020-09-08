3ca98b3f8a42ad2d202ee0495234f1f0e1d778da
Bienvenido Velez <Bienvenido.Velez@upr.edu>
2020-09-08 17:06:37 -0400

    Added two cars on opposite corners.

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -12,6 +12,23 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+        int xPos = 0;
+        int yPos = 0;
+		ofDrawRectangle(xPos+0,yPos+10,60,10);
+		ofDrawCircle(xPos+15,yPos+25,5);
+		ofDrawCircle(xPos+45,yPos+25,5);
+		ofDrawLine(xPos+10,yPos+10,xPos+20,yPos+0);
+		ofDrawLine(xPos+20,yPos+0,xPos+40,yPos+0);
+		ofDrawLine(xPos+40,yPos+0,xPos+50,yPos+10);
+
+        xPos = ofGetWidth() - 60;
+        yPos = ofGetHeight() - 40;
+		ofDrawRectangle(xPos+0,yPos+10,60,10);
+		ofDrawCircle(xPos+15,yPos+25,5);
+		ofDrawCircle(xPos+45,yPos+25,5);
+		ofDrawLine(xPos+10,yPos+10,xPos+20,yPos+0);
+		ofDrawLine(xPos+20,yPos+0,xPos+40,yPos+0);
+		ofDrawLine(xPos+40,yPos+0,xPos+50,yPos+10);
 
 }
 
